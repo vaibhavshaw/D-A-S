4b3ed9160ddf6306edae48eef45e67432fed0b69
Vaibhav Shaw <35895188+vaibhavshaw@users.noreply.github.com>
2019-05-13 01:55:36 +0530

    Update db_connect.php

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -1,11 +1,4 @@
 <?php
 $connection = mysqli_connect("ec2-54-235-208-103.compute-1.amazonaws.com","imwdiwqtnjfpgu","d8de5eea5972cc95f47c7e41b6b6ba3064e337860a61a7fc02fb346e00378554");
 mysqli_select_db($connection,'dfp1p5hjfiqida');
-if($connection->connect_error){
- die("failed");
-  
-}
-else{
-echo "Connected";
-}
 ?>
