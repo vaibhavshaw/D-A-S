91de31ea4a988b5ac011aee9d043b3daef30dabd
Vaibhav Shaw <35895188+vaibhavshaw@users.noreply.github.com>
2019-05-13 01:52:37 +0530

    Update db_connect.php

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -1,5 +1,11 @@
 <?php
 $connection = mysqli_connect("ec2-54-235-208-103.compute-1.amazonaws.com","imwdiwqtnjfpgu","d8de5eea5972cc95f47c7e41b6b6ba3064e337860a61a7fc02fb346e00378554");
 mysqli_select_db($connection,'india_post');
+if($connection->connect_error){
+ die("failed");
+  
+}
+else{
 echo "Connected";
+}
 ?>
