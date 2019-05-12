c6ec4d8b1a2e1ea253c2ca239fa7894c6f169e6d
Vaibhav Shaw <35895188+vaibhavshaw@users.noreply.github.com>
2019-05-13 01:32:50 +0530

    connect

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -1,4 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost","root","");
+$connection = mysqli_connect("ec2-54-235-208-103.compute-1.amazonaws.com","imwdiwqtnjfpgu","d8de5eea5972cc95f47c7e41b6b6ba3064e337860a61a7fc02fb346e00378554
+URI");
 mysqli_select_db($connection,'india_post');
 ?>
